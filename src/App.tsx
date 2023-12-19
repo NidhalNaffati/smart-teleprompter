3a9b3ca388ebf8504b6a31ce6a317ba3699bc6fc
NidhalNaffati <nidhalnaffati@gmail.com>
2023-12-19 16:45:34 +0100

    feat: Add react-webcam component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import "./styles/App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/electron-vite.animate.svg";
 import SpeechToTextAnalyzer from "./components/SpeechToTextAnalyzer.tsx";
 import VoskControl from "./components/VoskControl.tsx";
+import Webcam from "react-webcam";
 
 function App() {
   return (
     <>
-      <div>
-        <div>
-          <a href="https://electron-vite.github.io" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo"/>
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo"/>
-          </a>
-        </div>
-      </div>
-
-      <div>
+      <div className="vosk-control">
         <VoskControl/>
       </div>
-
+      <div>
+        <Webcam
+          audio={false}
+          mirrored={true}
+          height={240}
+          width={360}
+        />
+      </div>
       <div>
         <SpeechToTextAnalyzer/>
       </div>
