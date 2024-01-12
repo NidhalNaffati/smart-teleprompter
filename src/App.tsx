633f7f710bd7dcf01a090ec62e8a29766502df2e
NidhalNaffati <nidhalnaffati@gmail.com>
2024-01-12 16:42:59 +0100

    Refactor: Remove 'react-webcam'

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import "./styles/App.css";
 import SpeechToTextAnalyzer from "./components/SpeechToTextAnalyzer.tsx";
 import VoskControl from "./components/VoskControl.tsx";
-import Webcam from "react-webcam";
 
 function App() {
   return (
     <>
       <div className="vosk-control">
         <VoskControl/>
-      </div>
-      <div>
-        <Webcam
-          audio={false}
-          mirrored={true}
-          height={240}
-          width={360}
-        />
       </div>
       <div>
         <SpeechToTextAnalyzer/>
