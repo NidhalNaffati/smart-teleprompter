e0475fec5e6310dc97772f1e05fbaea0561d1232
NidhalNaffati <nidhalnaffati@gmail.com>
2024-03-30 17:32:18 +0100

    style: Update the Navbar's styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,39 @@ import {Link} from 'react-router-dom';
 
 export function Navbar() {
   return (
-    <nav className="bg-gray-800 py-4">
-      <ul className="flex">
-        <li className="mr-4">
-          <Link to="/" className="text-white hover:text-gray-300">Home</Link>
-        </li>
-        <li className="mr-4">
-          <Link to="/settings" className="text-white hover:text-gray-300">Settings</Link>
-        </li>
-        <li className="mr-4">
-          <Link to="/transcription" className="text-white hover:text-gray-300">Transcription</Link>
-        </li>
-      </ul>
+    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Link to="/home" className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Home</Link>
+        </a>
+        <button data-collapse-toggle="navbar-solid-bg" type="button"
+                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-solid-bg" aria-expanded="false">
+          <span className="sr-only">Open main menu</span>
+          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+               viewBox="0 0 17 14">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+          <ul
+            className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <li>
+              <Link to="/settings"
+                    className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent">
+                Settings
+              </Link>
+            </li>
+            <li>
+              <Link to="/transcription"
+                    className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent">
+                Transcription
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
