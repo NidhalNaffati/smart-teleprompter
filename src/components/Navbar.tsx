3b86bf9cbd3129bdd108cdbc8b2d4c2c66fe128e
NidhalNaffati <nidhalnaffati@gmail.com>
2024-04-03 11:53:09 +0100

    fix: Set the correct path to the home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,7 @@ export function Navbar() {
   return (
     <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Link to="/home" className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Home</Link>
-        </a>
+        <Link to="/" className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Home</Link>
         <button data-collapse-toggle="navbar-solid-bg" type="button"
                 className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="navbar-solid-bg" aria-expanded="false">
