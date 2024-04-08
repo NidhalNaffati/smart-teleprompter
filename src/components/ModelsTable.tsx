6ed080d607928039d58b4fe89760e1d7d7274e80
NidhalNaffati <nidhalnaffati@gmail.com>
2024-04-08 06:10:27 +0100

    fix the conflict

--- src/components/ModelsTable.tsx
+++ src/components/ModelsTable.tsx
@@ -50,9 +50,15 @@ const TableComponent = () => {
               :
               (
                 <td className="border px-4 py-2">
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                          onClick={() => handleDownload(item.URL)}>
-                    {item.Downloaded ? 'downloaded' : 'download'}
+                  <button
+                    className={`text-white font-bold py-2 px-4 rounded ${item.Downloaded ? 'bg-gray-500 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700'}`}
+                    onClick={() => {
+                      if (!item.Downloaded) {
+                        handleDownload(item.URL);
+                      }
+                    }}
+                    disabled={item.Downloaded}>
+                    {item.Downloaded ? 'Downloaded' : 'Download'}
                   </button>
                 </td>
               )
