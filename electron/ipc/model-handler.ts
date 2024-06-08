abfcb83b38871b6dcbab2849bd31af3c313a5316
NidhalNaffati <nidhalnaffati@gmail.com>
2024-06-08 15:59:00 +0100

    refactor: Create IPC_CHANNELS for the ipc communications

--- electron/ipc/model-handler.ts
+++ electron/ipc/model-handler.ts
@@ -1,16 +1,22 @@
 import {ipcMain} from "electron";
 import {deleteModel, downloadModel, listAvailableModels} from "../utils/model-files-handler.ts";
 
+const IPC_CHANNELS = {
+  DOWNLOAD_MODEL: 'download-model',
+  DELETE_MODEL: 'delete-model',
+  LIST_AVAILABLE_MODELS: 'list-available-models',
+};
+
 export function registerModelDownloadIPC() {
-  ipcMain.on('download-model', (_event, url, name) => {
+  ipcMain.on(IPC_CHANNELS.DOWNLOAD_MODEL, (_event, url, name) => {
     downloadModel(url, name);
   });
 
-  ipcMain.on('delete-model', (_event, name) => {
+  ipcMain.on(IPC_CHANNELS.DELETE_MODEL, (_event, name) => {
     deleteModel(name);
   });
 
-  ipcMain.handle('list-available-models', () => {
+  ipcMain.handle(IPC_CHANNELS.LIST_AVAILABLE_MODELS, () => {
     return listAvailableModels();
   });
 }
--- electron/ipc/vosk.ts
+++ electron/ipc/vosk.ts
@@ -1,15 +1,20 @@
-import { ipcMain, BrowserWindow } from "electron";
+import {ipcMain, BrowserWindow} from "electron";
 import childProcess from "child_process";
-import { startVoskProcess, stopVoskProcess } from "../utils/vosk-process/vosk-process.ts";
+import {startVoskProcess, stopVoskProcess} from "../utils/vosk-process/vosk-process.ts";
+
+const IPC_CHANNELS = {
+  START_RECOGNITION: "start-recognition",
+  STOP_RECOGNITION: "stop-recognition",
+};
 
 let voskProcess: childProcess.ChildProcess;
 
 export function registerVoskIPC(win: BrowserWindow) {
-	ipcMain.on("start-recognition", () => {
-		voskProcess = startVoskProcess(win);
-	});
+  ipcMain.on(IPC_CHANNELS.START_RECOGNITION, () => {
+    voskProcess = startVoskProcess(win);
+  });
 
-	ipcMain.on("stop-recognition", () => {
-		stopVoskProcess(voskProcess, win);
-	});
+  ipcMain.on(IPC_CHANNELS.STOP_RECOGNITION, () => {
+    stopVoskProcess(voskProcess, win);
+  });
 }
